f32fc9e2c062196e860dbc6fc5d9bc9c236a8976
Damian Rzeszot <damian.rzeszot@gmail.com>
2021-10-24 02:11:37 +0200

    Add `Codable`

--- Sources/Money/Money.swift
+++ Sources/Money/Money.swift
@@ -30,3 +30,30 @@ extension Money: Comparable {
   }
 }
 
+extension Money: Codable {
+  public enum Error: Swift.Error {
+    case invalidMoneyFormat
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let string = try container.decode(String.self)
+    let parts = string.split(separator: " ")
+
+    guard parts.count == 2 else { throw Error.invalidMoneyFormat }
+
+    let amountString = String(parts[0])
+    let currencyCodeString = String(parts[1])
+
+    guard let amount = Decimal(string: amountString) else { throw Error.invalidMoneyFormat }
+
+    self.amount = amount
+    self.currency = Currency(code: currencyCodeString)
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode("\(amount) \(String(currency))")
+  }
+}
+
--- Tests/MoneyTests/MoneyTests.swift
+++ Tests/MoneyTests/MoneyTests.swift
@@ -52,5 +52,44 @@ final class MoneyTests: XCTestCase {
     XCTAssertNotEqual(sut, Money(amount: 1.00, currency: "EUR"))
   }
 
+  // MARK: - Codable
+
+  func test_decodable() throws {
+    let json = """
+        "10000.00 USD"
+      """
+
+    let sut: Money = try decode(json)
+
+    XCTAssertEqual(sut.amount, 10_000)
+    XCTAssertEqual(sut.currency, "USD")
+  }
+
+  func test_encodable() throws {
+    let money = Money(amount: 20_000, currency: "EUR")
+
+    let sut = try encode(money)
+
+    XCTAssertEqual(sut, "\"20000 EUR\"")
+  }
+
+  // MARK: - Helpers
+
+  private func decode<T: Decodable>(_ string: String) throws -> T {
+    let data = string.data(using: .utf8)!
+    let decoder = JSONDecoder()
+
+    return try decoder.decode(T.self, from: data)
+  }
+
+  private func encode<T: Encodable>(_ value: T) throws -> String {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+
+    let data = try encoder.encode(value)
+
+    return String(data: data, encoding: .utf8) ?? ""
+  }
+
 }
 
